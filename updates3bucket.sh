3d18a9c895d052bc3af88a136435f897cf4793b3
Anthony Vattathil <avattathil@gmail.com>
2016-04-05 15:35:54 -0700

    changed s3 root

--- updates3bucket.sh
+++ updates3bucket.sh
@@ -7,12 +7,12 @@ TARGET=$PROD_TARGET
 
 pushcode () { 
    cd scripts
-   echo "ENDPOINT: $1/scripts"
+   echo "ENDPOINT: $1/"
    aws s3 sync scripts $1/ --exclude="*.git/*"
    cd ..
-   cd template
-   echo "ENDPOINT: $1/template"
-   aws s3 sync template $1/ --exclude="*.git/*"
+   cd templates
+   echo "ENDPOINT: $1/"
+   aws s3 sync templates $1/ --exclude="*.git/*"
    cd ..
 }
 
